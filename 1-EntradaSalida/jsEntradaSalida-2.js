e7d27a97e1c0216b32b57f7f2b11de5b5ed633e1
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-06 10:15:54 -0300

    Ejercicio 1 y 2
    Prompt, alert, var

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,19 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	nombre=prompt("ingrese nombre","natalia natalia");
+	alert("su nombre es: "+nombre);
+
+	//esto es un comentario de una linea
+
+	//alert(nombre);
+	/*
+	esto es un bloque
+	alert("nombre");
+	nombre="Daira";
+	alert(nombre);
+	*/
+    //var person = prompt("Please enter your name", "Harry Potter")
 }
 
