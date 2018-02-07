0959f984d74b29d158803c8ece399c981666de35
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-07 12:14:00 -0300

    6-10
    hechos en clase

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numeroUno;
+	var numeroDos;
+	var Resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	Resultado=numeroUno+numeroDos;
+
+	alert(Resultado);
 }
 
 function restar()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var Resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	Resultado=numeroUno-numeroDos;
+
+	alert(Resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno;
+	var numeroDos;
+	var Resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	Resultado=numeroUno*numeroDos;
+
+	alert(Resultado);
 }
 
 function dividir()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var Resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	Resultado=numeroUno/numeroDos;
+
+	alert(Resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,16 @@ en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
 
-	
+	var importe;
+	var resultado;
+
+	importe=document.getElementById('sueldo').value;
+
+	importe=parseInt(importe);
+
+	resultado=importe*1.1;
+
+	document.getElementById('resultado').value=resultado;
+
+
 }
