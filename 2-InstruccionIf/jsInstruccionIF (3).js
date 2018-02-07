05b92b3ab974116d9ee54af7210e0e6618128fe8
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-07 12:19:13 -0300

    3 ifs vistos en clase

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,41 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+
+	/*if(edad>=18)
+	{
+		alert("Sos mayor de edad");
+	}
+
+	if(edad<=17)
+	{
+		alert("Sos menor de edad");
+	}*///asi no porque es hacer mucho maneje// 
+
+	/*if(edad>=18)
+	{
+		alert("Sos mayor de edad");
+	}
+
+	if(edad<=17)
+	{
+		alert("Sos menor de edad");
+	}*/ //esto funciona pero puede dar error la forma mas facil es la de abajo //
+
+	if(edad>17)
+	{
+		alert("Sos mayor de edad");
+	}
+
+	else
+	{
+		alert("Sos menor de edad");
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
