6b55f671414f9bb241ec77bc4d56642418e6e7f3
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-06 23:29:36 -0300

    tp 1 y 2
    1 bien  2 no logre que funcionen botones

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,44 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+var largo;
+var ancho;
+var alambreacomprar;
+var resultado;
+
+largo=document.getElementById('Largo').value;
+ancho=document.getElementById('ancho').value;
+alambreacomprar=largo*ancho;
+resultado=alambreacomprar*3;
+
+alert("La cantidad de alambre a comprar es "+resultado);
+
+
 
 }
 function Circulo () 
 {
-	
+var radio;
+var resulcirculo;
+
+radio=document.getElementById('radio').value;
+resulcirculo=radio*3;
+
+alert("La cantidad de alambre a comprar"+resulcirculo);
+
 }
 function Materiales () 
 {
-	
-}
+var largo;
+var ancho;
+var terreno;
+var cemento;
+var cal;
+
+largo=document.getElementById('Largo').value;
+ancho=document.getElementById('ancho').value;
+terreno=largo*ancho;
+cemento=terreno*2;
+cal=terreno*3;
+
+alert("Para hacer el contrapiso necesitás "+cemento+" bolsas de cemento y "+cal+" bolsas de cal");
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,51 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var product1;
+	var product2;
+	var product3;
+	var resultadosum;
+
+	product1=document.getElementById('PrecioUno').value;
+	product2=document.getElementById('PrecioDos').value;
+	product3=document.getElementById('PrecioTres').value;
+	resultadosum=product1+product2+product3;
+
+	alert("La suma de los productos es: "+resultadosum);
+
+
+
 	
 }
 function Promedio () 
 {
-	
+	var product1;
+	var product2;
+	var product3;
+	var resultadosumaprom;
+	var resultadopromfinal;
+
+	product1=document.getElementById('PrecioUno').value;
+	product2=document.getElementById('PrecioDos').value;
+	product3=document.getElementById('PrecioTres').value;
+	resultadosumaprom=product1+product2+product3;
+	resultadopromfinal=resultadosumaprom/3;
+
+	alert("El promedio de los productos es: "+resultadopromfinal);
 }
 function PrecioFinal () 
 {
-	
+	var product1;
+	var product2;
+	var product3;
+	var resultadoiba;
+	var ibafinal
+
+	product1=document.getElementById('PrecioUno').value;
+	product2=document.getElementById('PrecioDos').value;
+	product3=document.getElementById('PrecioTres').value;
+	resultadoiba=product1+product2+product3;
+	ibafinal=resultadoiba*0.21;
+
+	alert("El iba de los productos es: "+ibafinal);
 }
