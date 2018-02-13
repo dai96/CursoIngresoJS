f9b95960a224f08b928f664efcf2969d8dfa1864
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-13 20:20:21 -0300

    1 2 3
    4 no salio

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,16 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
+	var importe
+	var final
+
+	importe=prompt("Importe");
+
+	importe=parseInt(importe);
+
+	final=importe*1.21;
+
+	document.getElementById('importe').value=final
 	
 }
 
--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,20 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var ancho
+	var largo
+	var perimetro
+	var final
+
+	ancho=document.getElementById('ancho').value;
+	largo=document.getElementById('largo').value;
+
+	perimetro=ancho*2+largo*2;
+	final=perimetro*6;
+
+
+
+	alert("Necesitas "+final+" metros de alambre.");
 	
 }
 
