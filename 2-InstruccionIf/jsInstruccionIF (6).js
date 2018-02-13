a588543f282509778cee34e1843f251a61ddc00b
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-13 20:16:37 -0300

    del 4 al 7 en casa
    el 7 no salió

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,24 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad
 
+	edad=document.getElementById('edad').value;
 
+	if (edad>17)
+	{
+		alert("Sos un Adulto");
+
+	}
+	if(edad>12 && edad<18)
+	{
+			alert("Sos adolescente");
+	}
+
+	if(edad<13)
+	{
+			alert("Sos un niño");
+	}
 
 
 }//FIN DE LA FUNCIÓN
