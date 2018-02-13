0da428cbe8ab44c50434437b25a2ffc70df0a1dc
dai96 <36193806+dai96@users.noreply.github.com>
2018-02-13 20:21:01 -0300

    1 2 3
    4 no salio

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,12 @@
 function Mostrar()
 {
-  
+  var importe;
+  var final;
+
+  importe=prompt("ingrese un importe");
+
+  final=importe*0.75;
+
+  document.getElementById('importeFinal').value=final
+
 }
--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,12 @@
 
 function Mostrar()
 {
+	var base;
+	var resultado;
 
+	base=document.getElementById('laBase').value;
+
+	resultado=base*base;
+
+	alert("La superficie es de "+resultado+" cemtímetros");
 }
