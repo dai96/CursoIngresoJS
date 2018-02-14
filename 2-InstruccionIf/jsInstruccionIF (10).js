e4dd963ce72e15e493d7880244d127a8d21154c0
Daira <36193806+dai96@users.noreply.github.com>
2018-02-14 11:04:54 -0300

    9 y 10 con random

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,41 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero
 	
+	numero=Math.floor(Math.random() * 10)+ 1;
 
+	if (numero>8)
+	{
+		alert(numero+" EXELENTE");
+	}
+	else
+	{
+		if (numero<4)
+		{
+			alert(numero+" Vamos, la proxima se puede");
+		}
+		else
+		{
+			alert(numero+" APROBÓ");
+		}
+	}
+
+
+	/*if (numero>8)
+	{
+		alert(numero+" EXELENTE");
+	}
+	
+	if (numero<4)
+	{
+		alert(numero+" Vamos, la proxima se puede");
+	}
+
+	if (numero<9 && numero>3)
+	{
+		alert(numero+" APROBÓ");
+	}*/
+
+	
 }//FIN DE LA FUNCIÓN
