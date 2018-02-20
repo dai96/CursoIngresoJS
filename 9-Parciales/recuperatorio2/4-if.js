7f1668da4ad96cb621171202863d2f972ed3dc14
Daira <36193806+dai96@users.noreply.github.com>
2018-02-20 07:25:05 -0300

    4 y 5

--- 9-Parciales/recuperatorio2/4-if.js
+++ 9-Parciales/recuperatorio2/4-if.js
@@ -1,7 +1,40 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
+
 {
 	alert("Funciona 4-if");
 	
+	var numeroUno;
+	var	numeroDos;
+	var suma;
+	var resta;
+	var multi;
+
+	numeroUno=prompt("Ingrese un número");
+	numeroDos=prompt("Ingrese otro número");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if (numeroUno==numeroDos)
+	{
+		multi=numeroUno*numeroDos;
+		alert(multi);
+	}
+	else 
+	{
+		if (numeroUno>numeroDos)
+		{
+			resta=numeroUno-numeroDos;
+			alert(resta);
+		}
+		else
+		{
+			suma=numeroUno+numeroDos;
+			alert(suma);
+		}
+	}	
+
 }
+
 
--- 9-Parciales/recuperatorio2/5-switch.js
+++ 9-Parciales/recuperatorio2/5-switch.js
@@ -1,7 +1,25 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
+
 {
 	alert("Funciona 5-Switch");
+
+	var mes;
+
+	mes=prompt("Ingrese el mes");
+
+	switch(mes)
+	{
+		case "enero":
+		case "Enero":
+		case "Febrero":
+		case "febrero":
+			alert("Veranito!!!");
+			break;
+		default:
+			alert ("extraño enero y febrero!!!");
+			break;	
+	}
 	
 }
 
