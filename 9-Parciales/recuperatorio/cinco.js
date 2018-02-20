27a0537f2c35eae290a8c06eff4f1dbc2c78cfd7
Daira <36193806+dai96@users.noreply.github.com>
2018-02-20 07:24:42 -0300

    4 y 5

--- 9-Parciales/recuperatorio/cinco.js
+++ 9-Parciales/recuperatorio/cinco.js
@@ -1,4 +1,21 @@
 function Mostrar()
 {
+	var meses
 
+	meses=prompt("Ingrese mes");
+
+	switch(meses)
+	{
+		case "Enero":
+		case "enero":
+			alert("comienza el año");
+			break;
+		case "Diciembre":
+		case "diciembre":
+			alert("Se vienen las fiestas");
+			break;
+		default:
+			alert("no es enero, ni diciembre");
+			break;		
+	}
 }
--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,42 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno=prompt("Ingrese número uno:");
+	numeroDos=prompt("Ingrese número dos");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	suma=numeroUno+numeroDos;
+
+	/*if (suma>0)
+	{
+		alert("Positivo");
+		if (suma==0)
+		{
+			alert("cero");
+		}
+		if (suma<0) Porque si aca pongo un else no funciona? No es que funcionara ahora, de todos modos, pero con else tmpcoo
+		{
+			alert("negativo");
+		}
+	}*/
+
+	if (suma==0)
+	{
+		alert("cero");
+	}
+	else
+	{
+		if(suma>0)
+		{
+			alert("positivo");
+		}
+		else
+		{
+			alert("negativo");/* no funciona :C*/
 
 }
