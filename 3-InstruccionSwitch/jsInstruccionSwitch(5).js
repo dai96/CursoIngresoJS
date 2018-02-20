b459939102d668abfe0bcee706fd2e5a97c41f98
Daira <36193806+dai96@users.noreply.github.com>
2018-02-20 07:22:46 -0300

    hechos 5 4 y 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -4,8 +4,28 @@ function Mostrar()
 var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
+	//*switch (laHora)
+	/*{
+		case laHora:
+			if (laHora>6 && laHora<12)
+			{
+				alert ("Es de mañana");
+			}
+			break;
+	}*/
 	
-	
+
+	switch (laHora)
+	{
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+			alert("Es de mañana");
+			break;
+
+	}
 
 
 
