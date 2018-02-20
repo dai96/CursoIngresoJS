4627ed2d84073bd856094cdcca0bb8f8f4514423
Daira <36193806+dai96@users.noreply.github.com>
2018-02-20 12:08:47 -0300

    seguir

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,22 +10,41 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	var lampara
- 	var cantidad
- 	var 
 
- 	if (cantidad>5)
+
+	Las lámparas están al mismo precio de $35 pesos final. 
+A.	Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % 
+y si es de otra marca el descuento es del 30%. 
+C.	Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % 
+y si es de otra marca el descuento es del 20%. 
+D.	Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz" el descuento es del 15%,
+ si es “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%. 
+E.	Si el importe final con descuento suma más de $120 se debe sumar un 10% 
+de ingresos brutos en informar del impuesto con el siguiente mensaje: ”IIBB Usted pago X”, siendo X el impuesto que se pagó
+
+
+ 	var lampara;
+ 	var cantidad;
+ 	var descuento;
+
+ 	switch (lampara)
  	{
- 		descuento=cantidad*0.5;
-	}
-	if (cantidad>4)
-	{
-		descuento=cantidad*0.7;
-	}
-
-	if (cantidad>4 && lampara="FelipeLamparas")
-	{
-		descuento=cantidad*0.6;
-	}
-	if ()
+ 		case "ArgentinaLuz"
+ 		if (cantidad==5)
+ 		{
+ 			descuento=35*0.40;
+ 		}
+ 		else 
+ 		{
+ 			if (cantidad==4)
+ 			{
+ 				descuento=35*0.20;
+ 			}
+ 			else
+ 			{
+ 				if()
+ 			}
+ 		}
+ 	}
 }	
