0f59a1e990d9066f00e4734b6dae77538037d9ac
Daira <36193806+dai96@users.noreply.github.com>
2018-02-28 12:08:23 -0300

    ejercicios en clase

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,83 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+	
+	//no validar y no while. Solo IF se ingresan dos n si son iguales se 
+	//concatenan si el mayor es el primero se multiplican de lo contrario
+	// se restan. si la multiplacacion es par, agregar la palabra es par
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var multi;
+	var resta;
+	var multipar;
+
+	numeroUno=prompt("Ingrese un número");
+	numeroDos=prompt("Ingrese otro número");
+
+	if (numeroDos==numeroUno)
+	{
+		suma=numeroUno+numeroDos;
+		alert("Su concatenación da "+suma);
+	}
+	else
+	{
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+		if (numeroUno>numeroDos)
+		{
+		 	multi=numeroUno*numeroDos;
+		 	
+		 	if (multi!=0)
+		 	{
+		 	
+		 		if (multi%2==0)
+		 		{
+		 			alert("Su multiplicación da "+multi+" y es par");
+		 		}
+		 		else 
+		 		{
+		 			alert("Su multiplicación da "+multi);
+		 		}
+			}
+				alert("Su multiplicación da "+multi);
+			
+		}
+		else
+		{
+		 	resta=numeroUno-numeroDos;
+		 	alert("Su resta da "+resta);
+		}
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*alert("Funciona 4-if");
 	var numeroUno;
 	var numeroDos;
 	var suma;
@@ -32,7 +108,7 @@ function Mostrar()
 			alert(suma);
 		}
 
-	}
+	}*/
 		
 }
 
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,103 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	//alert("Funciona 8-iteraciones");
+	//Se ingresa hasta que el usuario quiera nombre de bichos, peso del mismo
+	//validar que sea mas que 0
+	//temperatura del habitad de este bicho (entre menos 40 y mas 40)
+	//informar la cantidad de temperaturas pares/ impares
+	//el nombre del más obeso y del menos, cantidad de animales
+	//que vivene en habitad de 0 o menos, promedio de peso de todos los chobis
+	//temperatura mazima y minima
+
+	var interaciones=0;
+	var respuesta="si";
+	var nombre;
+	var peso;
+	var temperatura;
+	var pares=0;
+	var impares=0;
+	var nombreMax;
+	var nombreMin;
+	var pesoMax;
+	var pesoMin;
+	var promedio;
+	var tempMax;
+	var tempMin;
+	var bichosFrios=0;
+	var acumulador;
+
+
+	while (respuesta!="no")
+	{
+		interaciones++;
+		nombre=prompt("Ingrese un animal");
+		peso=prompt("Ingrese el peso");
+		peso=parseInt(peso);
+		while (peso<0 || isNan(peso))
+		{
+			peso=prompt("Ingrese el peso");
+			peso=parseInt(peso);
+		}
+		temperatura=prompt("Ingrese la temperatura del habitad")
+		temperatura=parseInt(temperatura);
+		while (temperatura<-40 || temperatura>40 || isNan(temperatura))
+		{
+			temperatura=prompt("Ingrese la temperatura del habitad")
+			temperatura=parseInt(temperatura);
+		}
+
+		
+		if (temperatura%2==0 && temperatura!=0)
+		{
+			pares++;
+		}
+		if (!(temperatura%2==0) && temperatura!=0)
+		{
+			impares++;
+		}
+		if (interaciones==1)
+		{
+			pesoMax=peso;
+			pesoMin=peso;
+			nombreMax=nombre;
+			nombreMin=nombre;
+			tempMax=temperatura;
+			tempMin=temperatura;
+		}
+		else
+		{
+			if (pesoMax<peso)
+			{
+				pesoMax=peso;
+				nombreMax=animal;
+			}
+			if (pesoMin>peso)
+			{
+				pesoMin=peso;
+				acumulaMin=animal;
+			}
+			if (tempMax<temperatura)
+			{
+				tempMax=temperatura;
+			}
+			if (tempMin>temperatura)
+			{
+				tempMin=temperatura;
+			}
+		}
+		if (temperatura<0)
+		{
+			bichosFrios++;
+		}
+		acumulador=acumulador+peso;
+		respuesta=prompt("Para salir ingrese no");
+	}
+	promedio=acumulador/interaciones;
+
+	document.write("Temperaturas <br>pares "+pares+"<br>impares "+impares+"<br>maxima "+tempMax+" C°<br>minima "+tempMin+" C°<br>Viven en habitad de 0 o menos "+bichosFrios+" animales <br>Peso<br>más pesado "+pesoMax+" Kg<br>menos pasado "+pesoMin+" Kg<br>promedio "+promedio+" Kg");
+}	
+	//el nombre del más obeso y del menos,
+	// promedio de peso de todos los chobis
 	
-}
 
